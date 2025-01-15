2d59bd13c15d0d2a2019c6ab00c7e157d60ef32c
Petar Vico <petarvico3@outlook.com>
2025-01-15 11:52:10 -0500

    create empty database creation/init scripts for dev and prod deploymentS

---
+++ devops/db/dev.sql
@@ -0,0 +1,4 @@
+-- Purpose: Instantiate a new database for development purposes.
+
+-- TODO: Fill in the SQL script accordingly
+
---
+++ devops/db/prod.sql
@@ -0,0 +1,4 @@
+-- Purpose: Instantiate a new database for production purposes.
+
+-- TODO: Fill in the SQL script accordingly
+
