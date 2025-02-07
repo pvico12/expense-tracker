79013ff3848054215bfac7f5ffa9043bcffede0c
Petar Vico <petarvico3@outlook.com>
2025-02-07 16:42:05 -0500

    create Users table with some basic fields

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,20 @@
 from sqlalchemy import Column, Integer, String
 from db import Base
 
-# Basic User model to check for database connectivity and interfacing
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    role = Column(String(50), nullable=False, default='user')
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
 
-    def __init__(self, name=None):
-        self.name = name
+    def __init__(self, username=None, password=None, firstname=None, lastname=None):
+        self.username = username
+        self.password = password
+        self.firstname = firstname
+        self.lastname = lastname
 
     def __repr__(self):
-        return f'<User {self.name!r}>'
+        return f'<User {self.username!r}>'
