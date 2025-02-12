3399d4c0b7b96ddce32377789ad8b3d87aa2331e
Nicole Planeta <60528262+Eternalon03@users.noreply.github.com>
2025-02-12 01:04:48 -0500

    refactor out of nav file

--- ExpenseTracker/app/src/main/java/com/cs446/expensetracker/nav/HomeNavContainer.kt
+++ ExpenseTracker/app/src/main/java/com/cs446/expensetracker/nav/HomeNavContainer.kt
@@ -1,236 +1,24 @@
 package com.cs446.expensetracker.nav
 
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RectF
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import android.graphics.Typeface
-import android.icu.text.DecimalFormat
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.cs446.expensetracker.mockData.dashboard_mock_expense
-import com.cs446.expensetracker.ui.ui.theme.*
-import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.MPPointF
-import com.github.mikephil.charting.formatter.ValueFormatter
+import com.cs446.expensetracker.dashboard.Dashboard
 
 
 class HomeNavContainer {
 
-    val format = DecimalFormat("#,###.00")
-
     @Composable
     fun HomeNavHost() {
         val homeNavController = rememberNavController()
-        val scrollState = rememberScrollState()
+        val dashboard = Dashboard()
 
         NavHost(homeNavController, startDestination = "home") {
             composable("home") {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(mainBackgroundColor)
-                        .verticalScroll(scrollState)
-                    ,
-                    verticalArrangement = Arrangement.Top
-                ) {
-                    Text(
-                        text = "Home Dashboard",
-                        color = mainTextColor,
-                        style = Typography.titleLarge,
-                        modifier = Modifier
-                            .padding(start = 16.dp, top = 16.dp)
-                    )
-                    Piechart()
-                    Text(
-                        text = "This Month's Expenses",
-                        color = mainTextColor,
-                        style = Typography.titleLarge,
-                        modifier = Modifier
-                            .padding(start = 16.dp, top = 16.dp)
-                    )
-                    Text(
-                        text = "Total Spending: $${format.format(dashboard_mock_expense.total_spending)}",
-                        color = mainTextColor,
-                        style = Typography.titleLarge,
-                        fontSize = 27.sp,
-                        modifier = Modifier
-                            .padding(start = 16.dp, top = 8.dp, bottom = 16.dp)
-                    )
-                    for(expense in dashboard_mock_expense.categories) {
-                        Card(
-                            modifier = Modifier
-                                .padding(4.dp)
-                                .fillMaxSize()
-                            ,
-                            elevation = CardDefaults.cardElevation(defaultElevation = 3.dp),
-                            colors = CardDefaults.cardColors(containerColor = tileColor)
-                        ) {
-                            Box (
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                            ) {
-                                Box(
-                                    modifier = Modifier
-                                        .size(40.dp)
-                                        .padding(start = 10.dp, top = 10.dp, bottom = 10.dp, end = 10.dp)
-                                        .clip(CircleShape)
-                                        .background(expense.customColor as Color)
-                                        .align(Alignment.CenterStart)
-                                )
-                                Text(
-                                    text = "${expense.category}: ",
-                                    color = mainTextColor,
-                                    style = Typography.labelSmall,
-                                    modifier = Modifier
-                                        .padding(start=40.dp,top=8.dp,bottom=8.dp,end=8.dp)
-                                )
-                                Spacer(Modifier.fillMaxWidth(0.2f))
-                                Text(
-                                    text = "${expense.percentage}%",
-                                    color = PurpleGrey40,
-                                    style = Typography.labelSmall,
-                                    modifier = Modifier
-                                        .padding(8.dp)
-                                        .align(Alignment.Center)
-                                )
-                                Text(
-                                    text = "$${format.format(expense.amount)}",
-                                    color = PurpleGrey40,
-                                    style = Typography.labelSmall,
-                                    modifier = Modifier
-                                        .padding(start=1.dp,top=8.dp,bottom=8.dp,end=20.dp)
-                                        .align(Alignment.CenterEnd)
-                                )
-                            }
-
-                        }
-                    }
-                }
+                dashboard.DashboardHost()
             }
         }
     }
 
-    @Composable
-    private fun Piechart() {
-        AndroidView(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(350.dp)
-                .padding(16.dp),
-            factory = { context ->
-                PieChart(context).apply {
-                    // Customize the PieChart here
-                    setExtraOffsets(5f, 10f, 5f, 5f)
-                    setDragDecelerationFrictionCoef(0.95f)
-                    getDescription().setEnabled(false)
-
-                    setDrawHoleEnabled(true)
-                    setHoleColor(mainBackgroundColor.toArgb())
-                    setTransparentCircleColor(tileColor.toArgb())
-                    setTransparentCircleAlpha(110)
-                    setHoleRadius(61f)
-                    setTransparentCircleRadius(70f)
-                    setDrawCenterText(true)
-
-                    // rotation of the pieChart by touch
-                    setRotationEnabled(true)
-                    setRotationAngle(0f)
-                    setHighlightPerTapEnabled(true)
-
-                    animateY(1400, Easing.EaseInOutQuad)
-
-                    legend.isEnabled = false
-
-                    // TODO: if we need more colors, add a color jitterer
-                    val colors: ArrayList<Int> = ArrayList()
-
-                    // on below line we are creating array list and
-                    // adding data to it to display in pie chart
-                    val entries: ArrayList<PieEntry> = ArrayList()
-                    for(expense in dashboard_mock_expense.categories) {
-                        entries.add(PieEntry(expense.percentage, expense.category))
-                        colors.add((expense.customColor as Color).toArgb())
-                    }
-
-                    setNoDataText("Add your first transactions to see the pie chart")
-
-                    // on below line we are setting pie data set
-                    val dataSet = PieDataSet(entries, "Mobile OS")
-
-                    dataSet.setDrawIcons(true)
-                    dataSet.iconsOffset = MPPointF(0f, 40f)
-                    dataSet.selectionShift = 5f
-                    dataSet.sliceSpace = 3f
-
-                    // on below line we are setting colors.
-                    dataSet.colors = colors
-
-                    // on below line we are setting pie data set
-                    val data = PieData(dataSet)
-                    data.setValueFormatter(categoryValueFormatter())
-                    data.setValueTextSize(1f)
-                    setEntryLabelColor(Color.Black.toArgb())
-                    setEntryLabelTextSize(18f)
-                    setEntryLabelTypeface(Typeface.DEFAULT_BOLD)
-                    data.setValueTextColor(Color.Black.toArgb())
-                    data.setValueTypeface(Typeface.DEFAULT_BOLD)
-                    setData(data)
-
-
-                }
-            },
-            update = { pieChart ->
-                // refresh
-                pieChart.invalidate()
-            }
-        )
-    }
-
-    class categoryValueFormatter : ValueFormatter() {
-        override fun getFormattedValue(value: Float): String {
-            return ""
-        }
-
-        fun drawBackground(canvas: Canvas, label: String, x: Float, y: Float, paint: Paint) {
-            val padding = 8f
-            val backgroundRect = RectF(x - padding, y - padding, x + paint.measureText(label) + padding, y + paint.textSize + padding)
-            paint.color = 0xFFDDDDDD.toInt() // Background color
-            canvas.drawRect(backgroundRect, paint)
-        }
-    }
 }
