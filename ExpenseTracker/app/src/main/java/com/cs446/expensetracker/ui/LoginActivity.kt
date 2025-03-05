d5eb414f570aa2ad405569435a6265158a4c90d1
Jennifer Wu <jw8wu@uwaterloo.ca>
2025-03-04 21:18:39 -0500

    enhance login page aesthetics

--- ExpenseTracker/app/src/main/java/com/cs446/expensetracker/ui/LoginActivity.kt
+++ ExpenseTracker/app/src/main/java/com/cs446/expensetracker/ui/LoginActivity.kt
@@ -5,15 +5,32 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.auth0.jwt.JWT
 import com.cs446.expensetracker.MainActivity
+import com.cs446.expensetracker.R
 import com.cs446.expensetracker.api.RetrofitInstance
 import com.cs446.expensetracker.api.models.LoginRequest
 import com.cs446.expensetracker.session.UserSession
@@ -21,17 +38,39 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import com.cs446.expensetracker.ui.ui.theme.mainTextColor
+import com.cs446.expensetracker.ui.ui.theme.tileColor
+import com.cs446.expensetracker.ui.ui.theme.Typography
+import com.cs446.expensetracker.ui.ui.theme.Pink40
+import com.cs446.expensetracker.ui.ui.theme.Purple40
+
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen(onLoginSuccess = {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            }, onCreateAccountClick = {
-                startActivity(Intent(this, SignupActivity::class.java))
-            })
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Pink40
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.coin_half),
+                        contentDescription = "Coin Image",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter),
+                    )
+                }
+                LoginScreen(onLoginSuccess = {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }, onCreateAccountClick = {
+                    startActivity(Intent(this, SignupActivity::class.java))
+                })
+            }
         }
     }
 }
@@ -42,55 +81,128 @@ fun LoginScreen(onLoginSuccess: () -> Unit, onCreateAccountClick: () -> Unit) {
     var password by remember { mutableStateOf("") }
     var isLoading by remember { mutableStateOf(false) }
     var errorMessage by remember { mutableStateOf<String?>(null) }
+    val buttonShape = RoundedCornerShape(26.dp)
+    val textFieldShape = RoundedCornerShape(12.dp)
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(48.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(250.dp))
+        Text(
+            text = "Welcome Back",
+            color = tileColor,
+            style = Typography.headlineLarge,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Login to your account",
+            style = Typography.bodySmall,
+            color = tileColor
+        )
+        Spacer(modifier = Modifier.height(64.dp))
         TextField(
             value = username,
             onValueChange = { username = it },
-            label = { Text("Username") }
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth(),
+            shape = textFieldShape,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
         )
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         TextField(
             value = password,
             onValueChange = { password = it },
             label = { Text("Password") },
-            visualTransformation = PasswordVisualTransformation()
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation(),
+            shape = textFieldShape,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            isLoading = true
-            errorMessage = null
-            CoroutineScope(Dispatchers.IO).launch {
-                val result = login(username, password)
-                withContext(Dispatchers.Main) {
-                    isLoading = false
-                    if (result) {
-                        onLoginSuccess()
-                    } else {
-                        errorMessage = "Login failed. Please try again."
+        Spacer(modifier = Modifier.height(180.dp))
+        Button(
+            onClick = {
+                isLoading = true
+                errorMessage = null
+                CoroutineScope(Dispatchers.IO).launch {
+                    val result = login(username, password)
+                    withContext(Dispatchers.Main) {
+                        isLoading = false
+                        if (result) {
+                            onLoginSuccess()
+                        } else {
+                            errorMessage = "Login failed. Please try again."
+                        }
                     }
                 }
-            }
-        }) {
-            if (isLoading) {
-                CircularProgressIndicator()
-            } else {
-                Text("Login")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .graphicsLayer {
+                    shadowElevation = 8f  // Elevation of the shadow
+                    shape = buttonShape    // Consistent shape for shadow
+                    clip = true            // Clip to the shape to prevent overflow
+                    translationX = 4f      // Move shadow right (adjust for more/less)
+                    translationY = 4f      // Move shadow down (adjust for more/less)
+                },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = tileColor
+            ),
+            contentPadding = PaddingValues(0.dp),
+            shape = buttonShape
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                mainTextColor,
+                                Purple40
+                            )
+                        ),
+                        shape = buttonShape
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                if (isLoading) {
+                    CircularProgressIndicator()
+                } else {
+                    Text(text="LOGIN", style=Typography.headlineSmall)
+                }
             }
         }
         errorMessage?.let {
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = it, color = MaterialTheme.colorScheme.error)
+            Text(text = it, color = Color(0xFFC08261))
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onCreateAccountClick) {
-            Text("Create Account")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Don't have an account? ")
+            TextButton(onClick = onCreateAccountClick) {
+                Text(
+                    text = "Sign up",
+                    color = tileColor,
+                    fontWeight = FontWeight.Bold,
+                    textDecoration = TextDecoration.Underline
+                )
+            }
         }
     }
 }
--- ExpenseTracker/app/src/main/java/com/cs446/expensetracker/ui/ui/theme/Type.kt
+++ ExpenseTracker/app/src/main/java/com/cs446/expensetracker/ui/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.cs446.expensetracker.ui.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cs446.expensetracker.R
+
+val migaeFamily = FontFamily(
+    Font(R.font.migae_semibold, FontWeight.SemiBold),
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,12 +22,33 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
         fontSize = 30.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = migaeFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 40.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = migaeFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 4.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
