3e47082488b8f3c0a2224792aa00b3d0365d254a
Petar Vico <petarvico3@outlook.com>
2025-03-27 01:25:55 -0400

    trigger assert on purpose

--- backend/test_deals.py
+++ backend/test_deals.py
@@ -42,6 +42,7 @@ def test_create_deal(server):
     }
     try:
         response = requests.post(url, json=data, headers=headers)
+        assert response.json() == False
         assert response.status_code == 201
         created_deal = response.json()
         TARGET_DEAL_ID = created_deal["id"]  # Store the created deal ID
